96415f1c81ac7755073d853c10cb1764c0ddb99c
Justin Green <justin98366@gmail.com>
2017-04-04 12:22:35 -0700

    added js, css, and html for lab

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('lulwhat this rocks!');
+
+var sushi = prompt('do you think i like sushi?');
+
+if (sushi.trim().toLowerCase() == 'yes' || sushi.trim().toLowerCase() == 'y' ){
+  alert('you were correct');
+}
+else{
+  alert("You are cluless about me!");
+}
